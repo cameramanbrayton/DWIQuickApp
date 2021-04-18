81d8d1b921c79c5ab44cce8d993e7156fe1775cb
cameramanbrayton <info@duelingworlds.com>
2021-04-17 23:23:20 -0500

    DWI Quick App09

--- app/src/main/java/com/dwiquickapp/duelingworldsinternationalquickapp/MainActivity.java
+++ app/src/main/java/com/dwiquickapp/duelingworldsinternationalquickapp/MainActivity.java
@@ -25,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.bseHomeTextView);
         textView.setMovementMethod(LinkMovementMethod.getInstance());
 
+        textView = findViewById(R.id.bsmeHomeTextView);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+
     }
 
     public void openBrowser(View view){
